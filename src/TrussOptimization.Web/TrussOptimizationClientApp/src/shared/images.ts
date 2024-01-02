c1151d34bb5ffc8890feb763d283a1636ceeab2c
mch <michal.zeg@gmail.com>
2024-01-02 19:47:35 +0100

    Fixed assets for images for trusses

--- src/TrussOptimization.Web/TrussOptimizationClientApp/src/shared/images.ts
+++ src/TrussOptimization.Web/TrussOptimizationClientApp/src/shared/images.ts
@@ -1,3 +1,6 @@
+import env from "@/core/env/env";
 
-export const getImageSVG = (img: string) => `/img/${img}.svg`
-
+export const getImageSVG = (img: string) =>
+  env
+    ? `/img/${img}.svg`
+    : require(`@/assets/${img}.svg`);
